778a9aeefc3e6c4baffd3b7791f2f63ad5330960
TranTienDat <datdc21v7n602@vlvh.ctu.edu.vn>
2025-10-01 17:11:37 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,12 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3001,
-  },
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      }
+    }
+  }
 })
